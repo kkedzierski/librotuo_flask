6ec63cac51507893eef85f3350fe1cd6052207bb
kkedzierski <kkedzierski@edu.cdv.pl>
2020-04-22 00:10:28 +0200

    Create website template

--- flask_Index.py
+++ flask_Index.py
@@ -1,17 +1,73 @@
-from flask import Flask, escape, request
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Tolkien',
+        'title': 'Lords of the Rings II two towers',
+        'content': '312 pages',
+        'data_published': 'June 18, 1975'
+    },
+    {
+        'author': 'Tolkien',
+        'title': 'Lords of the Rings II two towers',
+        'content': '312 pages',
+        'data_published': 'June 18, 1975'
+    },
+    {
+        'author': 'Tolkien',
+        'title': 'Lords of the Rings II two towers',
+        'content': '312 pages',
+        'data_published': 'June 18, 1975'
+    },
+    {
+        'author': 'Tolkien',
+        'title': 'Lords of the Rings II two towers',
+        'content': '312 pages',
+        'data_published': 'June 18, 1975'
+    },
+    {
+        'author': 'Tolkien',
+        'title': 'Lords of the Rings II two towers',
+        'content': '312 pages',
+        'data_published': 'June 18, 1975'
+    },
+    {
+        'author': 'Tolkien',
+        'title': 'Lords of the Rings II two towers',
+        'content': '312 pages',
+        'data_published': 'June 18, 1975'
+    },
+    {
+        'author': 'Tolkien',
+        'title': 'Lords of the Rings II two towers',
+        'content': '312 pages',
+        'data_published': 'June 18, 1975'
+    },
+    {
+        'author': 'Tolkien',
+        'title': 'Lords of the Rings II two towers',
+        'content': '312 pages',
+        'data_published': 'June 18, 1975'
+    },
+    {
+        'author': 'Tolkien',
+        'title': 'Lords of the Rings',
+        'content': '450 pages',
+        'data_published': 'April 10, 1970'
+    }
+
+]
+
 @app.route('/')
 @app.route('/home')
-def hello():
-    name = request.args.get("name", "Home Page")
-    return f'<h1>This is, {escape(name)}</h1>'
+def home():
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    name = request.args.get("name", "About Page")
-    return f'<h1>This is, {escape(name)}</h1>'
+     return render_template('about.html', title = 'about')
 
 if __name__ == '__main__':
     app.run(debug=True)
