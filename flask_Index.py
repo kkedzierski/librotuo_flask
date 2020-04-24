ebc000c81c2c4c91b4a44c7b266135de6cab783e
kkedzierski <kkedzierski@edu.cdv.pl>
2020-04-24 13:28:47 +0200

    Create login and registerfields with validation

--- flask_Index.py
+++ flask_Index.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '349eb206fa81e05f09ef955193bb062d'
 
 posts = [
     {
@@ -68,6 +71,25 @@ def home():
 @app.route('/about')
 def about():
      return render_template('about.html', title = 'about')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+     form = RegistrationForm()
+     if form.validate_on_submit():
+         flash(f'Account created for { form.username.data }!', 'success')
+         return redirect(url_for('home'))
+     return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+     form = LoginForm()
+     if form.validate_on_submit():
+         if form.email.data == 'admin@admin.com' and form.password.data == 'pass':
+             flash(f"Login successful", 'success')
+             return redirect(url_for('home'))
+         else:
+             flash(f'Login Unsuccessful. Please check your email and passoword', 'danger')
+     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
