fb54170ea75cede92cb8eeccad9a77f665362fd7
kkedzierski <kkedzierski@edu.cdv.pl>
2020-04-29 20:33:23 +0200

    hashed password functionality

--- librotuo/__init__.py
+++ librotuo/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '349eb206fa81e05f09ef955193bb062d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from librotuo import routes
-
