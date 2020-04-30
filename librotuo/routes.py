23f504777c360ab6b44f4e92ef679d2e4d3ae2dd
kkedzierski <kkedzierski@edu.cdv.pl>
2020-04-30 11:22:03 +0200

    create userauthorization to db with validation

--- librotuo/routes.py
+++ librotuo/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from librotuo import app, db, bcrypt
 from librotuo.forms import RegistrationForm, LoginForm
-from librotuo.models import User, Book
+from librotuo.models import User
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 @app.route('/')
@@ -17,6 +18,8 @@ def about():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -32,12 +35,29 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@admin.com' and form.password.data == 'pass':
-            flash(f"Login successful", 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password,
+                                               form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash(f'Login Unsuccessful. Please check your email and passoword',
+            flash('Login Unsuccessful. Please check your email and passoword',
                   'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='Your account')
